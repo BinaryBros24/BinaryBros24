0aa8feac72244f12362bb63bd031047d2d8090a4
BinaryBros24 <ojas.flori@outlook.de>
2025-04-26 16:25:07 +0300

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import tkinter as tk 
 import random
 
 # Funktion zum Würfeln
@@ -6,24 +6,24 @@ def wuerfeln():
     zahl = random.randint(1, 6)
     punkte_label.config(text=str(zahl))
     if zahl == 1:
-        nachricht_label.config(text="Miau")
+        nachricht_label.config(text="Try again")
     elif zahl == 2:
-        nachricht_label.config(text="Hello")
+        nachricht_label.config(text="You need more luck")
     elif zahl == 3:
         nachricht_label.config(text="Okay")
     elif zahl == 4:
-        nachricht_label.config(text="Gut")
+        nachricht_label.config(text="Good")
     elif zahl == 5:
-        nachricht_label.config(text="fast")
+        nachricht_label.config(text="Almost done")
     elif zahl == 6:
-        nachricht_label.config(text="Gewonnen")
+        nachricht_label.config(text="Winner")
 
 # GUI einrichten
 fenster = tk.Tk()
-fenster.title("Würfel Spiel")
+fenster.title("cube (no one asked for)")
 
 # Knopf zum Würfeln
-wuerfeln_knopf = tk.Button(fenster, text="Würfel", command=wuerfeln)
+wuerfeln_knopf = tk.Button(fenster, text="the cube is not cubing", command=wuerfeln)
 wuerfeln_knopf.pack()
 
 # Label für die Punkte
@@ -36,4 +36,5 @@ nachricht_label.pack()
 
 # Hauptschleife der GUI
 fenster.mainloop()
+
 
